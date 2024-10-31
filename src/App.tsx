3cfbc1dc2af567a3291f714eb30db6aa5c6cedb4
Zhuo FENG <fenprace.i@gmail.com>
2024-10-31 11:41:42 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="text-red-500">
+      <h1>Test UnoCSS preset-web-fonts</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ uno.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, presetUno, presetWebFonts } from "unocss";
+
+export default defineConfig({
+  cli: {
+    entry: {
+      patterns: ["src/**/*"],
+      outFile: "dist/index.css",
+    },
+  },
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        sans: [
+          {
+            name: "Roboto Flex",
+            weights: ["400", "600"],
+            italic: true,
+          },
+        ],
+      },
+    }),
+  ],
+});
